bf11fc9c73b2f6b3c0f1a9a6964d302761139c65
Selene Haruno <selene.haruno.ss@gmail.com>
2026-01-10 18:58:07 -0300

    use setup.py install_requires instead of requirements.txt

--- setup.py
+++ setup.py
@@ -8,10 +8,15 @@ setup(
 		"wikilinks",
 	],
 
+	install_requires=[
+		"mkdocs >= 1.6.1",
+		"regex >= 2025.11.3",
+		"setuptools >= 80.9.0",
+	],
+
 	entry_points = {
 		"mkdocs.plugins": [
 			"relative-wikilinks = plugin:RelativeWikilinks"
 		]
 	}
 )
-
